421e1b3cf24841f1ec83b6369fc239c6fd4be9d6
11akanksha <64818557+11akanksha@users.noreply.github.com>
2022-02-23 17:58:34 +0530

    Update 2.py

--- Python/2.py
+++ Python/2.py
@@ -22,3 +22,24 @@ message("def","4567","Justin")
 #O/p:
 Hello Justin , today is your lucky day!
 Error
+
+--------------------------------------------------------
+
+li = ["a","b","c"]
+
+def symbol_checker(fun):
+    def check(symbol,**kwargs):
+        if symbol in li:
+            fun(**kwargs)
+        else:
+            print("symbol mismatch")
+    return check
+    
+@ symbol_checker
+def show(**d):
+    print(d)
+
+show("a",key1 = "val1",key2="val2")
+
+#O/p:
+{'key1': 'val1', 'key2': 'val2'}
